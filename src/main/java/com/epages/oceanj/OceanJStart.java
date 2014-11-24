9e4f2e19167e316ae312f8c87d228c16a0233c15
Alessandro Brandimarti <a.brandimarti@epages.com>
2014-11-24 17:34:09 +0100

    configuration

--- src/main/java/com/epages/oceanj/OceanJStart.java
+++ src/main/java/com/epages/oceanj/OceanJStart.java
@@ -1,9 +1,16 @@
 package com.epages.oceanj;
 
+import org.apache.commons.configuration.Configuration;
+
+import com.epages.plugin.PluginServiceLoader;
+import com.epages.plugin.PluginServiceLoaderFactory;
+
 public class OceanJStart {
 
     public static void main(String[] args) {
-        System.out.println("OceanJStart.main()");
+        PluginServiceLoader psl = PluginServiceLoaderFactory.getInjector();
+        Configuration config = psl.getInstance(Configuration.class);
+        System.out.println(config.getString("OceanjServer.port"));
     }
 
 }
