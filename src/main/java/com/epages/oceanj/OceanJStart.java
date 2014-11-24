dc205a6b94b1db662505ace29f79b0e6d92cc1da
Alessandro Brandimarti <a.brandimarti@epages.com>
2014-11-24 16:37:33 +0100

    unit test

---
+++ src/main/java/com/epages/oceanj/OceanJStart.java
@@ -0,0 +1,9 @@
+package com.epages.oceanj;
+
+public class OceanJStart {
+
+    public static void main(String[] args) {
+        System.out.println("OceanJStart.main()");
+    }
+
+}
---
+++ src/test/java/com/epages/oceanj/TestMain.java
@@ -0,0 +1,12 @@
+package com.epages.oceanj;
+
+import org.junit.Test;
+
+public class TestMain {
+
+    @Test
+    public void testOceanJStart() {
+        OceanJStart.main(new String[]{});
+    }
+
+}
